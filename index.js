bb4b1a318f31dc47eef7c29347c1327ceccedd4a
Leonildo Camilo <leonildo_camilo@hotmail.com>
2024-01-03 20:23:21 -0300

    Conclusão de modulo handlebars

--- index.js
+++ index.js
@@ -3,11 +3,55 @@ const exphbs = require('express-handlebars')
 
 const app = express()
 
-app.engine('handlebars', exphbs.engine())
+const hbs = exphbs.create({
+  partialsDir: ['views/partials']
+})
+
+app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
+
 app.get('/dashboard', (req,res) => {
-  res.render('dashboard')
+  const items = ['item a', 'item b', 'item c']
+
+  res.render('dashboard',{items})
+})
+
+app.get('/post', (req, res) => {
+  const post = {
+    title: 'Alguma coisa',
+    category: 'Javascript',
+    body: 'Aprendendo js',
+    comments: 4
+  }
+
+  res.render('blogpost', {post})
+})
+
+app.get('/blog', (req,res) => {
+  const posts = [
+    {
+      title:'aprendendo node',
+      category:'js',
+      body:'teste',
+      comments: 7
+    },
+    {
+      title:'aprendendo c#',
+      category:'js',
+      body:'teste',
+      comments: 7
+    },
+    {
+      title:'aprendendo java',
+      category:'js',
+      body:'teste',
+      comments: 7
+    }
+  ]
+
+  res.render('blog', {posts})
 })
 
 app.get('/', (req, res) => {
